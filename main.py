4e83a0f35ceeaac603fb419d209e7f6d6755203f
Bernard Wiafe Akenteng <wiafe1713@gmail.com>
2025-06-20 04:13:18 -0700

    Update main.py

--- main.py
+++ main.py
@@ -86,7 +86,7 @@ def recognize_voice():
         st.warning("Sorry, I couldn’t understand that.")
     except sr.RequestError:
         st.error("Voice service failed.")
-    except Exception:
+    except Exception
 
 
 # --- Text-to-Speech ---
